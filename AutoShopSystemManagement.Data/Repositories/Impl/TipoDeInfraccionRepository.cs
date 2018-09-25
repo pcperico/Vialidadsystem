da957c8c3d84c821fc8fa75cfaedb461aad57225
Arturo Salinas <arturo.salinas@stepstone.com>
2018-09-25 13:36:38 -0600

    Revert "Remove ignored files"
    This reverts commit e6f6867a340cf3a75aa7b6c4a541b10e8a1b2d95.

---
+++ AutoShopSystemManagement.Data/Repositories/Impl/TipoDeInfraccionRepository.cs
@@ -0,0 +1,10 @@
+﻿using AutoShopSystemManagement.Data.Entities;
+using AutoShopSystemManagement.Data.Repositories.Interfaces;
+
+namespace AutoShopSystemManagement.Data.Repositories.Impl
+{
+    public class TipoDeInfraccionRepository: Repository<TipoDeInfraccion>,ITipoDeInfraccionRepository
+    {
+
+    }
+}
---
+++ VialidadSystem/App_Start/StructuremapMvc.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using StructureMap;
+using VialidadSystem.DependencyResolution;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(VialidadSystem.WebApplication.App_Start.StructuremapMvc), "Start")]
+
+namespace VialidadSystem.WebApplication.App_Start
+{
+    public static class StructuremapMvc
+    {
+        public static void Start()
+        {
+            var container = (IContainer)IoC.Initialize();
+            DependencyResolver.SetResolver(new SmDependencyResolver(container));
+        }
+    }
+}
